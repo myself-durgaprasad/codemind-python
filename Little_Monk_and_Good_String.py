e8a0f57aee5486e32d9b96a53fae0911552794cd
myself-durgaprasad <99956265+myself-durgaprasad@users.noreply.github.com>
2023-06-06 10:14:40 +0530

    n=input() l=[] v=('a','e','i','o','u') for i in range(len(n)):     c=0     for j in range(i,len(n)-1):         if n[j] in ('a','e','i','o','u') and n[j+1] in ('a','e','i','o','u'):             c+=1         else:             break     l.append(c) print(max(l)+1)

---
+++ Little_Monk_and_Good_String.py
@@ -0,0 +1,12 @@
+n=input()
+l=[]
+v=('a','e','i','o','u')
+for i in range(len(n)):
+    c=0
+    for j in range(i,len(n)-1):
+        if n[j] in ('a','e','i','o','u') and n[j+1] in ('a','e','i','o','u'):
+            c+=1
+        else:
+            break
+    l.append(c)
+print(max(l)+1)
